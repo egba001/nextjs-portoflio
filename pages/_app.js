be5e6ee0cf1b23878f006bf11c3bfa11a6ab7439
Nnaemeka <emekason128@gmail.com>
2023-06-07 01:03:11 +0100

    uninstalled react creative cursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import 'tailwindcss/tailwind.css';
 import { Layout } from '../components/Layout.js';
-import { Cursor } from 'react-creative-cursor';
-import 'react-creative-cursor/dist/styles.css';
 import { Navbar } from '../components/Navbar.js';
 import { Footer } from '../components/Footer.js';
 
@@ -10,8 +8,7 @@ import { Footer } from '../components/Footer.js';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Cursor isGelly={true} cursorSize={10} cursorBackgrounColor={'#9F2B68'} colorAnimationDuration={0.5} />
-      <Layout data-cursor-magnetic data-cursor-size="10px">
+      <Layout>
         <Navbar />
         <Component {...pageProps} />
         <Footer />
