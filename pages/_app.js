b38687897ac0f539d3d2ca340e5caacf8afeedbb
Nnaemeka <emekason128@gmail.com>
2023-06-08 11:08:35 +0100

    directory path rename

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import 'tailwindcss/tailwind.css';
-import { Layout } from '../components/Layout.js';
-import { Navbar } from '../components/Navbar.js';
-import { Footer } from '../components/Footer.js';
-
-// If loading a variable font, you don't need to specify the font weight
+import { Layout } from '../components/Layout.jsx';
+import { Navbar } from '../components/Navbar.jsx';
+import { Footer } from '../components/Footer.jsx';
 
 export default function App({ Component, pageProps }) {
   return (
