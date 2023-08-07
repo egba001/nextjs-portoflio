8a5e3551824f80bfb7b5b724eaaf9f881faa3e53
Nnaemeka <emekason128@gmail.com>
2023-08-07 15:49:20 +0100

    added image open graph meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,18 @@ export default function App({ Component, pageProps }) {
         <Head>
           <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
+          <title>Egba Nnaemeka's Portfolio</title>
           <meta name="theme-color" content="#000000" />
           <meta
               name="description"
               content="Web site created using create-react-app"
           />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content="@egba791" />
+          <meta property="og:url" content="http://egba.tech" />
+          <meta property="og:title" content="Egba Nnaemeka's Portfolio" />
+          <meta property="og:description" content="Portfolio site created using Next Js" />
+          <meta property="og:image" content="https://imgur.com/a/FSYe1wj" />
         </Head>
         <Navbar />
         <Component {...pageProps} />
