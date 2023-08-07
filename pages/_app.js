530ddc7ad09318d844a744dbd5aa363444f65ee5
Nnaemeka <emekason128@gmail.com>
2023-08-07 16:04:28 +0100

    Uploaded open graph image to cloudinary

--- pages/_app.js
+++ pages/_app.js
@@ -17,12 +17,12 @@ export default function App({ Component, pageProps }) {
               name="description"
               content="Web site created using create-react-app"
           />
-          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:creator" content="@egba791" />
           <meta property="og:url" content="http://egba.tech" />
           <meta property="og:title" content="Egba Nnaemeka's Portfolio" />
-          <meta property="og:description" content="Portfolio site created using Next Js" />
-          <meta property="og:image" content="https://imgur.com/a/FSYe1wj" />
+          <meta property="og:description" content="A frontend developer with a passion for crafting exceptional user experiences. With experience using React and JavaScript and Typescript, I specialize in building elegant and intuitive web applications. I'm excited to bring my expertise and enthusiasm to your next project, creating seamless and visually appealing experiences that leave a lasting impact." />
+          <meta property="og:image" content="https://res.cloudinary.com/da3qch8ch/image/upload/v1691420408/Nnaemeka_c1g0xj.jpg" />
         </Head>
         <Navbar />
         <Component {...pageProps} />
